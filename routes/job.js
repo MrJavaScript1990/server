b74953c169f2cfa90e9381a6df146d1003f04aa1
MrJavaScript1990 <parooein@gmail.com>
2019-07-20 10:33:45 +0700

    added API endpoint so client can add transfer job to the worker

--- routes/job.js
+++ routes/job.js
@@ -1,12 +1,20 @@
+/*
+        end point to get the jobs from user
+        and send them back the result
+
+        endpoint to give the authenticated users all the transactions
+*/
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 const newJob = require('./worker');
-const validateTransferInput = require('../validation/transfer');
-const Transaction = require('../models/Transaction');
+const validateTransferInput = require('../validation/transfer'); // transaction validator
+const Transaction = require('../models/Transaction');   // transaction schema
 
 router.post('/addjob', function (req, res) {
-
+    /*
+        this function checks if the new transaction is valid before send it to worker
+    */
     const {errors, isValid} = validateTransferInput(req.body);
 
     if (!isValid) {
@@ -17,20 +25,19 @@ router.post('/addjob', function (req, res) {
     let amount = req.body.amount;
     let currencyType = req.body.currencyType;
     let timeStampCreated = Date.now();
-    newJob({
+    newJob({        //send the job to worker
             srcUser,
             dstUser,
             amount,
             currencyType,
             timeStampCreated,
         }, (err) => {
-        if ((err.errText===''))
-            {
-                res.status(200).json({refNumber:err.transactionDetails.uid});
+            if ((err.errText === '')) {
+                res.status(200).json({refNumber: err.transactionDetails.uid}); //job completed successfully
             }
-           else{
+            else {
 
-                res.status(400).json({errText:err.errText,refNumber:err.transactionDetails.uid});
+                res.status(400).json({errText: err.errText, refNumber: err.transactionDetails.uid}); //job failed
             }
         }
     );
@@ -38,24 +45,25 @@ router.post('/addjob', function (req, res) {
 });
 router.post('/transactions', function (req, res) {
 
-
+    /*
+    this function checks if the user is in DB and then give back all the transactions
+    */
     let srcEmail = req.body.srcEmail.trim().toLowerCase();
-    //console.log(req.body);
     User.findOne({
         email: srcEmail,
-        uid:req.body.Uid,
+        uid: req.body.Uid,
     }).then(user => {
-        if(user) {
+        if (user) {
             Transaction.find({})
                 .then(data => {
                     res.status(200).json(data);
                 });
-        }else{
-            res.status(400).json({errText:'Wrong request pattern!'});
+        } else {
+            res.status(400).json({errText: 'Wrong request pattern!'});
         }
 
-    }).catch(err=>{
-        res.status(400).json({errText:'Wrong request pattern!'});
+    }).catch(err => {
+        res.status(400).json({errText: 'Wrong request pattern!'});
     })
 
 });
